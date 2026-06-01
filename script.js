e11566237ce5412629ce472ecb7c370793bdda10
Parasyte23 <chetanchoudhary264@gmail.com>
2026-06-01 09:07:53 +0530

    Add Calorie Calculation Logic

---
+++ script.js
@@ -0,0 +1,36 @@
+const foods = {
+    apple : 95,
+    banana : 105,
+    egg : 78,
+    roti : 120,
+    rice : 130
+};
+
+const foodInput = document.getElementById('food');
+const quanInput = document.getElementById('quantity');
+
+// console.log(foodInput);
+// console.log(quanInput);
+
+const submitbtn = document.getElementById('calculate');
+// console.log(submitbtn);
+
+const resultArea = document.getElementById('result-box');
+const result = document.getElementById('result');
+console.log(resultArea);
+console.log(result);
+// console.log(foods.apple);
+
+submitbtn.addEventListener('click' , () => {
+    currentFoodInput = foodInput.value;
+    currentQuanInput = quanInput.valueAsNumber;
+    // console.log(currentFoodInput);
+    // console.log(currentQuanInput);
+
+    if(currentQuanInput > 0) {
+        foodCalorie = foods[currentFoodInput];
+        calories = foodCalorie * currentQuanInput;
+        result.textContent = "The Total calorie Is " + calories;
+    }
+
+});
