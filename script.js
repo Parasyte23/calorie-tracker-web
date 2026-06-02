0e598305dc2a099f9a35f918eedddb2faa5db6fb
Parasyte23 <chetanchoudhary264@gmail.com>
2026-06-02 22:03:57 +0530

    Add New Row Button Logic

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const foods = {
     rice : 130
 };
 
+// For One Row Selection
 const foodInput = document.getElementById('food');
 const quanInput = document.getElementById('quantity');
-
 // console.log(foodInput);
 // console.log(quanInput);
 
@@ -36,5 +36,46 @@ submitbtn.addEventListener('click' , () => {
     } else {
         result.textContent = "Error : Please Enter Valid Food Item";
     }
+
+});
+
+// Add The Another Row Logic 
+
+const addBtn = document.getElementById('addRow');
+const inputRows = document.getElementById('food-row')
+// console.log(addBtn);
+// console.log(inputContainer);
+
+addBtn.addEventListener('click' , () => {
+    // Creating InputContainer
+    const newInputContainer = document.createElement('div');
+    newInputContainer.className = 'input-container';
+    
+    // Creating Food Input
+    const newInputFood = document.createElement('input');
+    newInputFood.type = 'text';
+    newInputFood.name = 'Food';
+    newInputFood.placeholder = 'Enter food item';
+    newInputFood.required = true;
+    newInputFood.className = 'food-quantity';
+
+    const newQuantityFood = document.createElement('input');
+    newQuantityFood.type = 'number';
+    newQuantityFood.name = 'Quantity';
+    newQuantityFood.placeholder = 'Enter Quantity';
+    newQuantityFood.required = true;
+    newQuantityFood.className = 'food-quantity';
+
+    // Creating Remove Button
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'removeRow';
+    removeBtn.innerText = '-';
+
+    inputRows.appendChild(newInputContainer);
+    newInputContainer.appendChild(newInputFood);
+    newInputContainer.appendChild(document.createTextNode(" "));
+    newInputContainer.appendChild(newQuantityFood);
+    newInputContainer.appendChild(document.createTextNode(" "));
+    newInputContainer.appendChild(removeBtn);
 
 });
